dd9dd81d0b868b83db5b569217b36a10a911eb80
Ilja <iljakristafenko@gmail.com>
2020-12-20 11:35:17 +0200

    Modal Lesson.

---
+++ starter/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+// console.log(btnsOpenModal);
+
+//TODO: Open modal window and overlay.--------------------------------------
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+//TODO: add click on all buttons.-------------------------------------------
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+//TODO:  Short function for ADD class.--------------------------------------
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+
+//TODO: Restore Class long way.---------------------------------------------
+// btnCloseModal.addEventListener("click", function () {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+// });
+// //! btnsOpenModal -- this is a pseudo Array , and it's don't have a methods.
+// overlay.addEventListener("click", function () {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+// });
+
+//TODO: ESC key.
+document.addEventListener("keydown", function (e) {
+    // console.log(e.key);
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
+
+
